da2ad700a349dabd1ef8b2ef1d79aa6103c8142f
Escanor1986 <lionel.zovi@gmail.com>
2023-06-23 10:25:14 +0200

    modifications nodemailer avec google-apis et token d'accès

--- back/app.js
+++ back/app.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const { google } = require("google-auth-library");
+const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
 const credentials = require("./Google_ID.json");
 const app = express();
+const OAuth2 = google.auth.OAuth2;
 
 app.use(express.json());
 
@@ -13,29 +14,47 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
-
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
-  res.setHeader("Acces-Control-Max-Age", 80000);
+  res.setHeader("Access-Control-Max-Age", 80000);
   next();
 });
 
-app.post("/submit-form", (req, res) => {
+app.post("/submit-form", async (req, res) => {
   const { firstName, lastName, email, birthdate, quantity, location } =
     req.body;
 
   console.log(req.body);
 
-  const authClient = new google.auth.OAuth2(
+  // Implémentation de la logique SMTP pour l'envoi d'un email de confirmation via Google avec OAuth2
+
+  // Création d'un client OAuth2 avec les identifiants client et secret
+  const authClient = new OAuth2(
     credentials.client_id,
     credentials.client_secret,
-    credentials.redirect_uris[0]
+    credentials.redirect_uris
   );
 
+  // Définition de la portée et de la redirection
+  const scopes = [
+    "https://mail.google.com/",
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.compose",
+  ];
+
+  // Obtenir un jeton d'accès
+  const url = authClient.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+
+  // Rediriger l'utilisateur vers l'URL d'autorisation générée
+  res.json({ url });
+
   const getToken = async (code) => {
-    const tokens = await authClient.getToken(code);
+    const { tokens } = await authClient.getToken(code);
     console.log("Jeton d'accès :", tokens.access_token);
     console.log("Jeton de rafraîchissement :", tokens.refresh_token);
 
@@ -58,38 +77,36 @@ app.post("/submit-form", (req, res) => {
       to: email,
       subject: "Confirmation d'inscription au prochain tournoi de GameOn !",
       html: `
-          <h1>Nouvelle inscription au tournoi de la part de :</h1>
-          <p>Prénom: ${firstName}</p>
-          <p>Nom: ${lastName}</p>
-          <p>E-mail: ${email}</p>
-          <p>Date de naissance: ${birthdate}</p>
-          <p>Nombre de tournois à votre actif: ${quantity}</p>
-          <p>Ville souhaitée pour le lieu du tournoi: ${location}</p>
-          <br />
-          <p>Cet E-mail en réponse vaut accusé de réception et de validation pour votre inscription au tournoi !</p>
-          <p>Conservez-le jusqu'au jour J !</p>
-          `,
+        <h1>Nouvelle inscription au tournoi de la part de :</h1>
+        <p>Prénom: ${firstName}</p>
+        <p>Nom: ${lastName}</p>
+        <p>E-mail: ${email}</p>
+        <p>Date de naissance: ${birthdate}</p>
+        <p>Nombre de tournois à votre actif: ${quantity}</p>
+        <p>Ville souhaitée pour le lieu du tournoi: ${location}</p>
+        <br />
+        <p>Cet E-mail en réponse vaut accusé de réception et de validation pour votre inscription au tournoi !</p>
+        <p>Conservez-le jusqu'au jour J !</p>
+      `,
     };
 
     // Envoi de l'e-mail
-    transporter
-      .sendMail(mailOptions)
-      .then((info) => {
-        console.log("E-mail envoyé :", info.response);
-        res.json({ success: true, message: "Formulaire soumis avec succès !" });
-      })
-      .catch((error) => {
-        console.error("Erreur lors de l'envoi de l'e-mail :", error);
-        res.status(500).json({
-          success: false,
-          message: "Une erreur est survenue lors de l'envoi de l'e-mail.",
-        });
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log("E-mail envoyé :", info.response);
+      res.json({ success: true, message: "Formulaire soumis avec succès !" });
+    } catch (error) {
+      console.error("Erreur lors de l'envoi de l'e-mail :", error);
+      res.status(500).json({
+        success: false,
+        message: "Une erreur est survenue lors de l'envoi de l'e-mail.",
       });
+    }
   };
 
   // Utilisez le code d'autorisation reçu dans la requête de redirection pour obtenir les jetons
   const authorizationCode = "CODE_D_AUTORISATION";
-  getToken(authorizationCode);
+  await getToken(authorizationCode);
 });
 
 module.exports = app;
