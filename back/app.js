cd236406e35f901da7883f25cdde769d0a1a3fab
Escanor1986 <lionel.zovi@gmail.com>
2023-07-05 11:20:20 +0200

    last app.js fix

--- back/app.js
+++ back/app.js
@@ -27,22 +27,20 @@ app.post("/submit-form", async (req, res) => {
 
   // Implémentation de la logique SMTP pour l'envoi d'un email de confirmation via Google avec OAuth2
 
-  const getToken = async (code) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "lionel.zovi@gmail.com",
+      pass: process.env.PASSWORD,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: 'lionel.zovi@gmail.com',
-        pass: '<<mot de passe généréner par ton MFA>>'
-      },
-    });
-
-    // Options de l'e-mail
-    const mailOptions = {
-      from: 'lionel.zovi@gmail.com', // Sender address
-      to: 'lionel.zovi@gmail.com', // Recipient address
-      subject: "Confirmation d'inscription au prochain tournoi de GameOn !",
-      html: `
+  // Options de l'e-mail
+  const mailOptions = {
+    from: "lionel.zovi@gmail.com", // Sender address
+    to: "lionel.zovi@gmail.com", // Recipient address
+    subject: "Confirmation d'inscription au prochain tournoi de GameOn !",
+    html: `
         <h1>Nouvelle inscription au tournoi de la part de :</h1>
         <p>Prénom: ${firstName}</p>
         <p>Nom: ${lastName}</p>
@@ -54,21 +52,20 @@ app.post("/submit-form", async (req, res) => {
         <p>Cet E-mail en réponse vaut accusé de réception et de validation pour votre inscription au tournoi !</p>
         <p>Conservez-le jusqu'au jour J !</p>
       `,
-    };
-
-    // Envoi de l'e-mail
-    try {
-      const info = await transporter.sendMail(mailOptions);
-      console.log("E-mail envoyé :", info.response);
-      res.json({ success: true, message: "Formulaire soumis avec succès !" });
-    } catch (error) {
-      console.error("Erreur lors de l'envoi de l'e-mail :", error);
-      res.status(500).json({
-        success: false,
-        message: "Une erreur est survenue lors de l'envoi de l'e-mail.",
-      });
-    }
   };
+
+  // Envoi de l'e-mail
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("E-mail envoyé :", info.response);
+    res.json({ success: true, message: "Formulaire soumis avec succès !" });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'e-mail :", error);
+    res.status(500).json({
+      success: false,
+      message: "Une erreur est survenue lors de l'envoi de l'e-mail.",
+    });
+  }
 });
 
 module.exports = app;
